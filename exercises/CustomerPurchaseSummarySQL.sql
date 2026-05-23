d3e39d7890ad78c3091a0af0c03146833c8b48f7
Nikhilkumar-Work <atnikhilkumar23@gmail.com>
2026-05-23 13:04:11 +0530

    ✅ Add solution for: Customer Purchase Summary - SQL

---
+++ exercises/CustomerPurchaseSummarySQL.sql
@@ -0,0 +1,11 @@
+SELECT
+id AS order_id,
+ROUND(total_amount, 2) AS total_amount,
+CAST (ordered_at AS CHAR(19))AS ordered_at
+FROM
+orders
+WHERE
+order_status = 'Delivered'
+AND total_amount > 150.00
+ORDER BY
+ordered_at DESC;
